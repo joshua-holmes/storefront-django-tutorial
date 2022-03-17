0b266f3e7abedc4ee0267767ca48e1a03142b982
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2022-03-17 17:49:00 -0500

    Add views with routes

--- playground/urls.py
+++ playground/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
   path("hello/", views.say_hello),
+  path('', views.index, name='index'),
+  path('<int:question_id>/', views.detail),
+  path('<int:question_id>/results/', views.results),
+  path('<int:question_id>/vote/', views.vote)
 ]
